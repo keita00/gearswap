2a1c36c86c13757566c3d7dc9171af5af99f8362
keita00 <78788946+keita00@users.noreply.github.com>
2025-05-04 11:10:37 +0200

    Update COR.lua

--- COR.lua
+++ COR.lua
@@ -64,3 +64,52 @@ end
 windower.register_event('prerender', function()
     handle_prerender()
 end)
+
+-- Full COR rolls list
+local roll_info = {
+    ["Corsair's Roll"] = {lucky=5, unlucky=9},
+    ["Chaos Roll"] = {lucky=4, unlucky=8},
+    ["Hunter's Roll"] = {lucky=4, unlucky=8},
+    ["Samurai Roll"] = {lucky=2, unlucky=6},
+    ["Rogue's Roll"] = {lucky=5, unlucky=9},
+    ["Allies' Roll"] = {lucky=3, unlucky=10},
+    ["Tactician's Roll"] = {lucky=5, unlucky=8},
+    ["Magus's Roll"] = {lucky=4, unlucky=8},
+    ["Healer's Roll"] = {lucky=4, unlucky=8},
+    ["Craftman's Roll"] = {lucky=5, unlucky=9},
+    ["Drachen's Roll"] = {lucky=4, unlucky=8},
+    ["Warlock's Roll"] = {lucky=4, unlucky=8},
+    ["Storm's Roll"] = {lucky=5, unlucky=9},
+    ["Fellow's Roll"] = {lucky=5, unlucky=9}
+}
+
+-- Handle COR rolls: Add all roll names and track results
+windower.register_event('action', function(act)
+    if act.category == 6 and act.actor_id == windower.ffxi.get_player().id then
+        local roll_id = act.param
+        local roll_name = res.job_abilities[roll_id] and res.job_abilities[roll_id].name
+        if roll_info[roll_name] then
+            local val = act.targets[1].actions[1].param % 12 + 1
+            local info = roll_info[roll_name]
+            if val == info.lucky then
+                windower.add_to_chat(32, ('>> Lucky Roll! [%s] = %d <<'):format(roll_name, val))
+            elseif val == info.unlucky then
+                windower.add_to_chat(123, ('>> Unlucky Roll. [%s] = %d <<'):format(roll_name, val))
+            else
+                windower.add_to_chat(200, ('>> %s = %d <<'):format(roll_name, val))
+            end
+        end
+    end
+end)
+
+-- Timer logic for all COR rolls
+function handle_cor_roll(spell)
+    if spell.type == 'CorsairRoll' then
+        equip(sets.rolls)
+        local duration = 300
+        local roll_name = spell.english
+        send_command('timers delete \"' .. roll_name .. '\"')
+        send_command('timers create \"' .. roll_name .. '\" ' .. duration .. ' down abilities/00255.png')
+        windower.add_to_chat(200, '>> Roll: ' .. roll_name .. ' (' .. duration .. 's) <<')
+    end
+end
